46f9bff0a146f2fc14db717a6fc97ccd1f7cf403
Justin Ahinon <justiny.ahinon@gmail.com>
2026-02-12 15:06:56 +0800

    Update index.tsx

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,4 +1,18 @@
 import { render } from "@opentui/solid";
 import { App } from "./app.js";
+import { clearConfig, loadConfig } from "./auth/config.js";
+
+const subcommand = process.argv[2];
+
+if (subcommand === "logout") {
+  const config = await loadConfig();
+  if (!config) {
+    console.log("Not logged in.");
+  } else {
+    await clearConfig();
+    console.log("Logged out successfully.");
+  }
+  process.exit(0);
+}
 
 render(() => <App />);
